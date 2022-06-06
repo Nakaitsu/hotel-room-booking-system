ffc989bea88c61f99df50e40b3156281adc70ac1
Nakaitsu <vitoremail.66@gmail.com>
2022-06-06 19:24:08 -0300

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Prototype.Models;
+
+namespace Prototype.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index(int? id)
+    {
+        return View(id);
+    }
+}
